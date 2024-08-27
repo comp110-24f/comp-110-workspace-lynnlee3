e5d7d530139b6b5187660d6a612d0b447d667b1e
Lynn Lee <lynnlee3@ad.unc.edu>
2024-08-27 13:46:29 -0400

    CQ00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,15 @@
+__author__ = "730485520"
+
+
+def mimic(message: str) -> str:
+    """Returns message to user."""
+    return message
+
+
+def main():
+    """Main function."""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
